a9e34a326dd478f7605b8d44cbc7c47389606c65
liyechen <gliyechen@hotmail.com>
2019-04-19 18:12:16 +0800

    init math/vector2

---
+++ core/math/Vector2.cpp
@@ -0,0 +1,11 @@
+#include "Vector2.h"
+
+Vector2::Vector2() {
+	x = 0.0;
+	y = 0.0;
+}
+
+Vector2::Vector2(float x, float y) {
+	x = x;
+	y = y;
+}
---
+++ core/math/Vector2.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+class Vector2 {
+private:
+	float x;
+	float y;
+public:
+	Vector2();
+	Vector2(float x, float y);
+};
