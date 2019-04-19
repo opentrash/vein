27ff74acb720cc1f1ada735baba4d02a043e75df
liyechen <gliyechen@hotmail.com>
2019-04-19 18:20:26 +0800

    comments

--- core/math/Vector2.cpp
+++ core/math/Vector2.cpp
@@ -1,10 +1,18 @@
 #include "Vector2.h"
 
+/**
+ * constructor with no input
+ * (0.0, 0.0)
+ */
 Vector2::Vector2() {
 	x = 0.0;
 	y = 0.0;
 }
 
+/**
+ * constructor with init
+ * (x, y)
+ */
 Vector2::Vector2(float x, float y) {
 	x = x;
 	y = y;
