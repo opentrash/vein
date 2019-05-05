c0ecda9008d0ad13c0fbbee0ceb3106236162d14
liyechen <gliyechen@hotmail.com>
2019-05-05 17:44:46 +0800

    rename vector3

--- test/math/vector3_test.h
+++ test/math/vector3_test.h
@@ -19,4 +19,22 @@ TEST(VECTOR3, INIT)
 	EXPECT_FLOAT_EQ(a.z, 0.0);
 }
 
+TEST(VECTOR3, COORD)
+{
+	Vector3 a;
+	a.x = 4.1;
+	a.y = 4.2;
+	EXPECT_FLOAT_EQ(a.coord[0], 4.1);
+	EXPECT_FLOAT_EQ(a.coord[1], 4.2);
+}
+
+TEST(VECTOR3, COPY)
+{
+	Vector3 a(3.0, 4.0, 5.0);
+	Vector3 b(a);
+	bool isTrue = b.x == 3.0 && b.y == 4.0 && b.z == 5.0;
+
+	EXPECT_TRUE(isTrue);
+}
+
 #endif /* !VECTOR3_TEST_H */
