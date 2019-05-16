1700075515239bd29790de6c359199d5413cd7ec
liyechen <gliyechen@hotmail.com>
2019-05-16 17:12:54 +0800

    math_funcs: almost_equal -> is_floating_point

--- core/math/math_funcs.h
+++ core/math/math_funcs.h
@@ -12,7 +12,7 @@
 #include <limits>
 
 template<class T>
-typename std::enable_if<!std::numeric_limits<T>::is_integer, bool>::type
+typename std::enable_if<std::is_floating_point<T>::value, bool>::type
 almost_equal(T x, T y, int ulp) {
     // the machine epsilon has to be scaled to the magnitude of the values used
     // and multiplied by the desired precision in ULPs (units in the last place)
